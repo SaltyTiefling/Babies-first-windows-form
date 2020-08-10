470f30a5c0de54b6b79e12b3a94489d058e28d4f
Jonas Van Mullem <jonasvanmullem@hotmail.com>
2020-08-10 12:42:50 +0200

    finsihed introduction

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,14 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblMijnLabel = new System.Windows.Forms.Label();
             this.btnButton1 = new System.Windows.Forms.Button();
+            this.cbFirstCombo = new System.Windows.Forms.ComboBox();
+            this.lbFirstListBox = new System.Windows.Forms.ListBox();
+            this.tbxNewItem = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblMijnLabel
             // 
+            this.lblMijnLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblMijnLabel.AutoSize = true;
-            this.lblMijnLabel.Location = new System.Drawing.Point(39, 35);
+            this.lblMijnLabel.Location = new System.Drawing.Point(74, 35);
             this.lblMijnLabel.Name = "lblMijnLabel";
             this.lblMijnLabel.Size = new System.Drawing.Size(50, 13);
             this.lblMijnLabel.TabIndex = 0;
@@ -44,21 +51,70 @@
             // 
             // btnButton1
             // 
+            this.btnButton1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.btnButton1.Location = new System.Drawing.Point(42, 51);
             this.btnButton1.Name = "btnButton1";
-            this.btnButton1.Size = new System.Drawing.Size(75, 23);
+            this.btnButton1.Size = new System.Drawing.Size(141, 24);
             this.btnButton1.TabIndex = 1;
             this.btnButton1.Text = "Klick mij!";
             this.btnButton1.UseVisualStyleBackColor = true;
             this.btnButton1.Click += new System.EventHandler(this.btnButton1_Click);
             // 
+            // cbFirstCombo
+            // 
+            this.cbFirstCombo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbFirstCombo.FormattingEnabled = true;
+            this.cbFirstCombo.Location = new System.Drawing.Point(42, 81);
+            this.cbFirstCombo.Name = "cbFirstCombo";
+            this.cbFirstCombo.Size = new System.Drawing.Size(142, 21);
+            this.cbFirstCombo.TabIndex = 2;
+            this.cbFirstCombo.SelectedIndexChanged += new System.EventHandler(this.cbFirstCombo_SelectedIndexChanged);
+            // 
+            // lbFirstListBox
+            // 
+            this.lbFirstListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbFirstListBox.FormattingEnabled = true;
+            this.lbFirstListBox.Location = new System.Drawing.Point(42, 109);
+            this.lbFirstListBox.Name = "lbFirstListBox";
+            this.lbFirstListBox.Size = new System.Drawing.Size(141, 134);
+            this.lbFirstListBox.TabIndex = 3;
+            // 
+            // tbxNewItem
+            // 
+            this.tbxNewItem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxNewItem.Location = new System.Drawing.Point(42, 249);
+            this.tbxNewItem.Name = "tbxNewItem";
+            this.tbxNewItem.Size = new System.Drawing.Size(141, 20);
+            this.tbxNewItem.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(42, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 26);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Toevoegen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(227, 356);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxNewItem);
+            this.Controls.Add(this.lbFirstListBox);
+            this.Controls.Add(this.cbFirstCombo);
             this.Controls.Add(this.btnButton1);
             this.Controls.Add(this.lblMijnLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -71,6 +127,10 @@
 
         private System.Windows.Forms.Label lblMijnLabel;
         private System.Windows.Forms.Button btnButton1;
+        private System.Windows.Forms.ComboBox cbFirstCombo;
+        private System.Windows.Forms.ListBox lbFirstListBox;
+        private System.Windows.Forms.TextBox tbxNewItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -31,12 +31,36 @@ namespace Babies_first_windows_form
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            cbFirstCombo.Items.Add("jonas");
+            cbFirstCombo.Items.Add("nog eens jonas");
+            cbFirstCombo.Items.Add("jonas again");
+            cbFirstCombo.Items.Add("jonas wants your lovin\'");
+            cbFirstCombo.Items.Add("jonas is bored but in a list woohoo");
+            foreach (var item in cbFirstCombo.Items)
+            {
+                lbFirstListBox.Items.Add(item);
+            }
         }
 
         private void lblMijnLabel_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void cbFirstCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblMijnLabel.Text = cbFirstCombo.SelectedItem.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbFirstListBox.Items.Add(tbxNewItem.Text);
+            cbFirstCombo.Items.Clear();
+            foreach (var item in lbFirstListBox.Items)
+            {
+                cbFirstCombo.Items.Add(item);
+            }
+        }
+
     }
 }
